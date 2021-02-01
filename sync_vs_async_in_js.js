e189c1b0b4241d3f858aa48d6118855901f180c1
quyen Luc <ancucchocon@gmail.com>
2021-02-01 14:04:24 +0700

    Update example 2 (event loop)

--- sync_vs_async_in_js.js
+++ sync_vs_async_in_js.js
@@ -31,4 +31,59 @@
 // Final
 
 
-// Example 2
+// Example Event loop multi thread with setTimeout () ansync in marco task in Event loop
+console.log("First !")
+
+// Function second() call after x000000 mili s
+setTimeout(function second(){
+    console.log('Time out !')
+}, 00000)
+// |        |
+// |        |
+// |        |
+// |        |
+// |        |
+// |        | => when func first call -> first call push in stack
+// |  First |   and execute 
+// |________|
+// Call Stack
+
+
+// |        |
+// |        |
+// |        |
+// |        |
+// |        |
+// |        | => when func finished -> second push into stack and encounter time
+// | second |   push event queue
+// |________|
+// Call Stack
+
+//  _________________________
+//         |
+//  second |
+// ________|________________
+// Event queue
+
+
+// |        |
+// |        |
+// |        |
+// |        |
+// |        |
+// |        | => Final push into call stack no need second exits
+// | Final  |   
+// |________|
+// Call Stack
+
+// Then -> Event Queue push into stack -> print (Timeout)
+console.log("Final !")
+
+
+//Output print
+// First !
+// Final !
+// Time out !
+
+
+
